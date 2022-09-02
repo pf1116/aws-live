2a63de8dc0f25a2f8a5c555bd3a9a92799cec4f4
pf1116 <65616833+pf1116@users.noreply.github.com>
2022-09-02 16:47:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c2o3fs4pfuov.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limpf-employee"
+customregion = "us-east-1"
